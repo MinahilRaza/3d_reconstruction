72405e6db3f8529d6809153788839f8a3972d858
Raisul Kibria <raisul.inc@gmail.com>
2023-02-12 14:10:42 +0100

    Add files via upload

---
+++ src/tutorial/create_mesh.py
@@ -0,0 +1,9 @@
+import open3d as o3d
+
+if __name__ == "__main__":
+    knot_data = o3d.data.KnotMesh()
+    print(f"Reading mesh from file: knot.ply stored at {knot_data.path}")
+    mesh = o3d.io.read_triangle_mesh(knot_data.path)
+    print(mesh)
+    print("Saving mesh to file: copy_of_knot.ply")
+    o3d.io.write_triangle_mesh("copy_of_knot.ply", mesh)
